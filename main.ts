fca2cc977962181542b6e69a6a9abf80fe10d38a
KOUSER BIBI <mechezaz@gmail.com>
2026-06-04 00:06:47 +0530

    Really Bad Bad Bad Bug Appeared.

--- main.ts
+++ main.ts
@@ -2,12 +2,6 @@ radio.onReceivedString(function (receivedString) {
     input.calibrateCompass()
     led.toggle(Math.map(90, 89, 24000, 90, 4), Math.sqrt(81))
 })
-let Counter = 0
-radio.setTransmitPower(978)
 basic.forever(function () {
     led.toggle(randint(0, 4), randint(0, 10))
-    Counter += 1
-    if (Math.randomBoolean()) {
-        radio.sendString("" + (Counter))
-    }
 })
