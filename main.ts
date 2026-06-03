de25659adc9fe63986e67551fbfc48240a9b4458
KOUSER BIBI <mechezaz@gmail.com>
2026-06-03 22:27:25 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@ radio.onReceivedString(function (receivedString) {
     input.calibrateCompass()
     led.toggle(Math.map(90, 89, 24000, 90, 4), Math.sqrt(81))
 })
-radio.setTransmitPower(978)
 let Counter = 0
+radio.setTransmitPower(978)
 basic.forever(function () {
     led.toggle(randint(0, 4), randint(0, 10))
     Counter += 1
