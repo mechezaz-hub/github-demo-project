d9cdd6eb22ed61a6cec3a3cca8589b5df614980d
Yellowjacketmod <mechezaz@gmail.com>
2026-06-06 16:26:59 +0530

    Logo added.

--- main.ts
+++ main.ts
@@ -2,9 +2,12 @@ radio.onReceivedString(function (receivedString) {
     input.calibrateCompass()
     led.toggle(Math.map(90, 89, 24000, 90, 4), Math.sqrt(81))
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendString("" + (Count))
+})
 let Count = 0
+Count = 0
 basic.forever(function () {
     led.toggle(randint(0, 4), randint(0, 10))
     Count += 1
-    radio.sendString("" + (Count))
 })
